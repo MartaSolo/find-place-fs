cd42d7c62a81e1662e74d0d54251353b332dcca2
MartaSolo <marciamarcia@o2.pl>
2022-06-19 20:15:47 +0200

    Edited PlaceDetailsHeader and package.json

--- src/components/PlaceDetails/PlaceDetailsHeader/PlaceDetailsHeader.js
+++ src/components/PlaceDetails/PlaceDetailsHeader/PlaceDetailsHeader.js
@@ -30,22 +30,3 @@ PlaceDetailsHeader.propTypes = {
 };
 
 export default connect(null, { resetPlacePhotos })(PlaceDetailsHeader);
-
-// import { Link } from "react-router-dom";
-// import { Typography } from "@mui/material";
-// import "./PlaceDetailsHeader.scss";
-
-// const PlaceDetailsHeader = () => {
-//   return (
-//     <header className="header__place">
-//       <div className="header__place--title">
-//         <Typography variant="h1">Find a place near you</Typography>
-//         <Link to="/" className="header__place--link">
-//           Home
-//         </Link>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default PlaceDetailsHeader;
